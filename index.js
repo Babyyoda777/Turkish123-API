b13b50157fe33b59b324e6fa0cd1d78b7a6fb33a
Muhammad Shah <80623330+Babyyoda777@users.noreply.github.com>
2024-03-27 14:06:53 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+// handler.js
+
+const Turkish123 = require('./Turkish123');
+
+const turkish123 = new Turkish123();
+
+module.exports.handler = async (req, res) => {
+    const { method, query, path } = req;
+
+    switch (path) {
+        case '/info': {
+            const { id } = query;
+            await turkish123.info(id);
+            res.json(turkish123.info);
+            break;
+        }
+        case '/getStream': {
+            const { episodeurl, episodenumber } = query;
+            const streamurl = await turkish123.getStream(episodeurl, episodenumber);
+            res.json({ streamurl });
+            break;
+        }
+        case '/search': {
+            const { query: searchQuery } = query;
+            await turkish123.search(searchQuery);
+            res.json(turkish123.SearchResults);
+            break;
+        }
+        case '/trending': {
+            await turkish123.trending();
+            res.json(turkish123.Trending);
+            break;
+        }
+        default: {
+            res.status(404).json({ error: 'Not Found' });
+        }
+    }
+};
